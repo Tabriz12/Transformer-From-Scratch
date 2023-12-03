7eec565123fb8dc54ee6354440e698bc62a69bf7
Tabriz Nuruyev <nuriyevtebriz@gmail.com>
2023-12-03 13:33:29 +0400

    config

--- config.py
+++ config.py
@@ -1,7 +1,22 @@
 
+# Model parameters
+
 embedding_dim = 512
-d_model = 64
+d_k_q = 64
+d_v = 64
 attention_heads = 8
 encoder_stack = 6
 decoder_stack = 6
-batch_size = 16
+batch_size = 12
+
+#Data
+
+dataset = 'wmt16'
+subset = 'tr-en'
+tokenizer = 'cl100k_base'
+voc_size = 100277
+
+
+#training
+device = 'cuda'
+epoch = 10
